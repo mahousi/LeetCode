15535f92b1085a742371e71ab534c16c698df199
mahousi <15680522065@163.com>
2018-11-30 00:08:40 +0800

    Update Array Partition I.java

--- code/Array Partition I.java
+++ code/Array Partition I.java
@@ -20,3 +20,51 @@ public class Solution {
 	}
 	
 }
+
+
+//second solution
+class Solution {
+    public int arrayPairSum(int[] nums) {
+        quickSortHelp(nums);
+        int sum = 0;
+        for(int i:nums)System.out.println(i);
+        for(int i=0;i<(nums.length+1)/2;i++){
+            sum = sum+nums[2*i];
+        }
+        return sum;
+    }
+    
+    public void quickSortHelp(int[] nums){
+        quickSort(nums,0,nums.length-1);
+    }
+    
+    public void quickSort(int[] nums,int low,int high){
+        int par = partition(nums,low,high);
+        if(low > high) return;
+        quickSort(nums,low,par-1);
+        quickSort(nums,par+1,high);
+    }
+    
+    public int partition(int[] nums,int low,int high){
+        while(low<high){
+            while(nums[high]>=nums[low]&&low<high){
+                high--;
+            }
+            swap(nums,low,high);
+            while(nums[low]<=nums[high]&&low<high){
+                low++;
+            }
+            swap(nums,low,high);
+        }
+        return low;
+    }
+    
+    public void swap(int nums[],int low,int high){
+        int tmp = nums[low];
+        nums[low]= nums[high];
+        nums[high]=tmp;
+        //nums[low] = nums[low] ^ nums[high];
+        //nums[high] = nums[low] ^ nums[high];
+        //nums[low] = nums[low] ^ nums[high];
+    }
+}
