80237f6fa94ecfbd7d588abae4ddb7e72d66eb41
mahousi <15680522065@163.com>
2018-12-06 18:18:05 +0800

    Update Monotonic Array.java

--- code/Monotonic Array.java
+++ code/Monotonic Array.java
@@ -13,3 +13,12 @@ class Solution {
         return inc && dec ?false:true;
     }
 }
+
+    public boolean isMonotonic(int[] A) {
+        boolean inc = true, dec = true;
+        for (int i = 1; i < A.length; ++i) {
+            inc &= A[i - 1] <= A[i];
+            dec &= A[i - 1] >= A[i];
+        }
+        return inc || dec;
+    }
