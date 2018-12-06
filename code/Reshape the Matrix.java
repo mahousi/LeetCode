6e57b0e0fae697e3a4c6a6c148387e8107dff59b
mahousi <15680522065@163.com>
2018-12-06 18:14:22 +0800

    Update Reshape the Matrix.java

--- code/Reshape the Matrix.java
+++ code/Reshape the Matrix.java
@@ -22,20 +22,11 @@ class Solution {
     }
 }
 
-class Solution {
-    public int[][] matrixReshape(int[][] nums, int r, int c) {
-        int m = nums.length;
-        int n = nums[0].length;
-        int row = 0,column = 0;
-        int res[][] = new int[r][c];
-        int count = 0;
-        if(r*c != m*n)  return nums;
-        for(int i = 0;i<m;i++){
-            for(int j = 0;j<n;j++){
-                res[count/c][count%c] = nums[i][j];
-                count++;
-            }
-        }
-        return res;
-    }
+public int[][] matrixReshape(int[][] nums, int r, int c) {
+    int n = nums.length, m = nums[0].length;
+    if (r*c != n*m) return nums;
+    int[][] res = new int[r][c];
+    for (int i=0;i<r*c;i++) 
+        res[i/c][i%c] = nums[i/m][i%m];
+    return res;
 }
